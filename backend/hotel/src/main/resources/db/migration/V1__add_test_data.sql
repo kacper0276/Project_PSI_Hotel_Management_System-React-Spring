7b6905bbaecaf9f08d34b7271c5d9fb74230224d
kacper0276 <kacper0276@op.pl>
2024-05-17 20:20:26 +0200

    Usuniecie niewłasiwych rezerwacji

--- backend/hotel/src/main/resources/db/migration/V1__add_test_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO platnosci(data_platnosci, kwota, metoda_platnosci, status_platnosci) VALUES ("2024-05-15", 100, "Karta", "Zaplacone");
-
-INSERT INTO klienci(email, haslo, imie, nazwa_firmy, nazwisko, nip, rodzaj, rola) VALUES("Test@test.pl", "haslo", "Adam", NULL, "Adamski", NULL, "KlientIndywidualny", "Klient");
-
-INSERT INTO pokoje(cena, data_zwolnienia, dostepnosc, ile_osob, typ_pokoju, wyposazenie) VALUES(250, "2024-05-15", 0, 4, "Fajny", "Dobre");
-INSERT INTO pokoje(cena, data_zwolnienia, dostepnosc, ile_osob, typ_pokoju, wyposazenie) VALUES(250, NULL, 1, 2, "Inny ale tez dobry", "Wspaniale");
