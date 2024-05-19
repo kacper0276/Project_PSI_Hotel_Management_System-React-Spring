bdbecdb92ee7d06210279ec1b773196836405949
kacper0276 <kacper0276@op.pl>
2024-05-20 00:57:24 +0200

    Fix walidacji

--- backend/hotel/src/main/java/psi/projekt/hotel/entity/Uzytkownicy.java
+++ backend/hotel/src/main/java/psi/projekt/hotel/entity/Uzytkownicy.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,10 +24,9 @@ public class Uzytkownicy {
 
     @NotNull
     @Email
-    @Min(5)
     private String email;
 
-    @Min(8)
+    @Size(min = 8, message = "Hasło musi zawierać co najmniej 8 znaków")
     private String haslo;
 
     @Enumerated(EnumType.STRING)
