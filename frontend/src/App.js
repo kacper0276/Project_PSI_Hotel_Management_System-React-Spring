8fac17d57f93f0013e6a6592fd38943229639b6a
kacper0276 <kacper0276@op.pl>
2024-05-21 16:03:52 +0200

    [FE] Dodanie strony do zmiany hasła i logowania

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,40 @@
-import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Header from './Layout/Header/Header';
-import Footer from './Layout/Footer/Footer'
-import Layout from './Layout/Layout'
-import MainPage from './Pages/MainPage/MainPage';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Layout/Header/Header";
+import Footer from "./Layout/Footer/Footer";
+import Layout from "./Layout/Layout";
+import MainPage from "./Pages/MainPage/MainPage";
+import ForgotPasswordPage from "./Pages/ForgotPassword/ForgotPasswordPage/ForgotPasswordPage";
+import LoginPage from "./Pages/LoginPage/LoginPage";
 
 function App() {
   const header = (
     <Header>
       <p>Header</p>
     </Header>
-  )
+  );
 
   const content = (
     <>
       <Routes>
-        <Route path='/' exact element={<MainPage />} />
+        <Route path="/" exact element={<MainPage />} />
+        <Route path="/zaloguj" exact element={<LoginPage />} />
+        <Route
+          path="/zmiana/:username"
+          exact
+          element={<ForgotPasswordPage />}
+        />
       </Routes>
     </>
-  )
+  );
 
-  const footer = <Footer />
+  const footer = <Footer />;
 
   return (
     <Router>
       <Layout header={header} content={content} footer={footer} />
     </Router>
-  ) 
+  );
 }
 
 export default App;
