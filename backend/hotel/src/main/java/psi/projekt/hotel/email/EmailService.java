832a7bdf53ac727569656ac70203e8b01674f60a
kacper0276 <kacper0276@op.pl>
2024-05-26 09:14:19 +0200

    [BE] Email Service

---
+++ backend/hotel/src/main/java/psi/projekt/hotel/email/EmailService.java
@@ -0,0 +1,49 @@
+package psi.projekt.hotel.email;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.core.io.Resource;
+import psi.projekt.hotel.config.EmailConfiguration;
+import psi.projekt.hotel.entity.Uzytkownicy;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final EmailConfiguration emailConfiguration;
+
+    @Value("${front.url}")
+    private String frontendUrl;
+
+    @Value("classpath:static/mail-aktywuj.html")
+    private Resource activeTemplate;
+    @Value("classpath:static/resetuj-haslo.html")
+    private Resource recoveryTemplate;
+
+
+    public void sendActivation(Uzytkownicy user){
+        try{
+            String html = Files.toString(activeTemplate.getFile(), Charsets.UTF_8);
+            html = html.replace("https://google.com",frontendUrl+"/aktywuj/"+user.getId());
+            emailConfiguration.sendMail(user.getEmail(), html,"Aktywacja konta",true);
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendPasswordRecovery(Uzytkownicy user,String uid){
+        try{
+            String html = Files.toString(recoveryTemplate.getFile(), Charsets.UTF_8);
+            html = html.replace("https://google.com",frontendUrl+"/odzyskaj-haslo/"+uid);
+            emailConfiguration.sendMail(user.getEmail(), html,"Odzyskanie hasła",true);
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+}
