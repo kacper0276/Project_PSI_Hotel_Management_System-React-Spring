bf19b055345106d5fda76bf0fffa9d50f4ac9995
kacper0276 <kacper0276@op.pl>
2024-06-02 02:31:16 +0200

    [FE] Sprawdzenie czy na koncie użytkownika stworzony jest już klient

--- frontend/src/Pages/UserPanel/CreateClient/CreateClient.js
+++ frontend/src/Pages/UserPanel/CreateClient/CreateClient.js
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import useWebsiteTitle from "../../../hooks/useWebsiteTitle";
 import styles from "./CreateClient.module.css";
 import axios from "axios";
 import { API_URL } from "../../../App";
+import MainContext from "../../../context/MainContext";
 
 export default function CreateClient() {
   useWebsiteTitle("Stwórz klienta");
+  const context = useContext(MainContext);
   const [clientData, setClientData] = useState({
     imie: null,
     nazwisko: null,
@@ -16,6 +18,7 @@ export default function CreateClient() {
       email: window.localStorage.getItem("email"),
     },
   });
+  const [clientCreated, setClientCreated] = useState(false);
 
   const createClient = async (e) => {
     e.preventDefault();
@@ -51,65 +54,90 @@ export default function CreateClient() {
     }
   };
 
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/klienci/czy-klient-istnieje/${context.state.userName}`)
+      .then((res) => {
+        if (res.data.message === "Istnieje") {
+          setClientCreated(true);
+        } else {
+          setClientCreated(false);
+        }
+      });
+  }, []);
+
   return (
     <div className={`${styles.main_container}`}>
-      <h2>Stwórz klienta</h2>
-      <form className={`${styles.form}`}>
-        <input
-          type="radio"
-          name="typeClient"
-          id="KlientIndywidualny"
-          value={"KlientIndywidualny"}
-          onChange={(e) =>
-            setClientData({ ...clientData, rodzaj: e.target.value })
-          }
-        />
-        <label htmlFor="KlientIndywidualny">Klient Indywidualny</label>
-        <input
-          type="radio"
-          name="typeClient"
-          id="KlientBizesowy"
-          value={"KlientBizesowy"}
-          onChange={(e) =>
-            setClientData({ ...clientData, rodzaj: e.target.value })
-          }
-        />
-        <label htmlFor="KlientBizesowy">Klient Biznesowy</label>
-        {clientData.rodzaj === "KlientBizesowy" ? (
-          <>
+      {clientCreated ? (
+        <>
+          <h2>
+            Stworzono użytkownika na podanym koncie: {context.state.userName}
+            Usuń obecnego klienta i stwórz nowego, ponieważ na jedno konto
+            przypada jeden klient
+          </h2>
+          <button>Usuń klienta</button>
+        </>
+      ) : (
+        <>
+          <h2>Stwórz klienta</h2>
+          <form className={`${styles.form}`}>
             <input
-              placeholder="Podaj NIP"
+              type="radio"
+              name="typeClient"
+              id="KlientIndywidualny"
+              value={"KlientIndywidualny"}
               onChange={(e) =>
-                setClientData({ ...clientData, nip: e.target.value })
+                setClientData({ ...clientData, rodzaj: e.target.value })
               }
             />
+            <label htmlFor="KlientIndywidualny">Klient Indywidualny</label>
             <input
-              placeholder="Podaj nazwę firmy"
+              type="radio"
+              name="typeClient"
+              id="KlientBizesowy"
+              value={"KlientBizesowy"}
               onChange={(e) =>
-                setClientData({ ...clientData, nazwaFirmy: e.target.value })
+                setClientData({ ...clientData, rodzaj: e.target.value })
               }
             />
-          </>
-        ) : (
-          <>
-            <input
-              placeholder="Podaj imię"
-              onChange={(e) =>
-                setClientData({ ...clientData, imie: e.target.value })
-              }
-            />
-            <input
-              placeholder="Podaj nazwisko"
-              onChange={(e) =>
-                setClientData({ ...clientData, nazwisko: e.target.value })
-              }
-            />
-          </>
-        )}
-        <button onClick={createClient} className={`${styles.send_button}`}>
-          Stwórz klienta
-        </button>
-      </form>
+            <label htmlFor="KlientBizesowy">Klient Biznesowy</label>
+            {clientData.rodzaj === "KlientBizesowy" ? (
+              <>
+                <input
+                  placeholder="Podaj NIP"
+                  onChange={(e) =>
+                    setClientData({ ...clientData, nip: e.target.value })
+                  }
+                />
+                <input
+                  placeholder="Podaj nazwę firmy"
+                  onChange={(e) =>
+                    setClientData({ ...clientData, nazwaFirmy: e.target.value })
+                  }
+                />
+              </>
+            ) : (
+              <>
+                <input
+                  placeholder="Podaj imię"
+                  onChange={(e) =>
+                    setClientData({ ...clientData, imie: e.target.value })
+                  }
+                />
+                <input
+                  placeholder="Podaj nazwisko"
+                  onChange={(e) =>
+                    setClientData({ ...clientData, nazwisko: e.target.value })
+                  }
+                />
+              </>
+            )}
+            <button onClick={createClient} className={`${styles.send_button}`}>
+              Stwórz klienta
+            </button>
+          </form>
+        </>
+      )}
     </div>
   );
 }
