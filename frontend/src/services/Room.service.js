dea2a8b3a9ed1c2d9dc38d9de917a296268d626c
kacper0276 <kacper0276@op.pl>
2024-06-06 09:49:27 +0200

    [FE]

--- frontend/src/services/Room.service.js
+++ frontend/src/services/Room.service.js
@@ -1,1 +1,16 @@
-export default class RoomService {}
+import axios from "axios";
+import { API_URL } from "../App";
+
+export default class RoomService {
+  static async getAllRooms() {
+    const response = await axios.get(`${API_URL}/pokoje`);
+
+    return response.data;
+  }
+
+  static async getRoomDetails(id) {
+    const response = await axios.get(`${API_URL}/pokoje/${id}`);
+
+    return response.data;
+  }
+}
