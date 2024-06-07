ca212511d67d0f87dd153dd51176a2f8874828f1
kacper0276 <kacper0276@op.pl>
2024-06-07 09:17:37 +0200

    [FE] Create method to finding room offers

--- frontend/src/services/Room.service.js
+++ frontend/src/services/Room.service.js
@@ -16,4 +16,17 @@ export default class RoomService {
   static async deleteRoom(id) {
     await api.delete(`/pokoje/${id}`);
   }
+
+  static async findRoomOffers(dateFrom, dateTo, roomType, persons) {
+    const response = await api.get("/pokoje/szukaj-ofert", {
+      params: {
+        dateFrom: dateFrom,
+        dateTo: dateTo,
+        roomType: roomType,
+        persons: persons,
+      },
+    });
+
+    return response.data;
+  }
 }
