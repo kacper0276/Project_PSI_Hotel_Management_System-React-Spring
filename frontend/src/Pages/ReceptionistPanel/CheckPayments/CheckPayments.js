52956dc3bbb8230737fd27c68e2718de03c654ad
kacper0276 <kacper0276@op.pl>
2024-06-08 13:06:56 +0200

    [FE] Use payment service in CheckPayments

--- frontend/src/Pages/ReceptionistPanel/CheckPayments/CheckPayments.js
+++ frontend/src/Pages/ReceptionistPanel/CheckPayments/CheckPayments.js
@@ -1,8 +1,19 @@
 import styles from "./CheckPayments.module.css";
 import useWebsiteTitle from "../../../hooks/useWebsiteTitle";
+import { useEffect, useState } from "react";
+import PaymentService from "../../../services/Payment.service";
 
 export default function CheckPayments() {
   useWebsiteTitle("Sprawdź płatności");
+  const [payments, setPayments] = useState([]);
+
+  const fetchPayments = async () => {
+    setPayments(await PaymentService.getAllPayments());
+  };
+
+  useEffect(() => {
+    fetchPayments();
+  }, []);
 
   return <div className={`${styles.main_container}`}>Sprawdź płatności</div>;
 }
