8a989a388db6393989cc7f14d4f957ebfe7a75fd
kacper0276 <kacper0276@op.pl>
2024-06-08 13:36:47 +0200

    [FE] Add method in RoomService to create new room

--- frontend/src/services/Room.service.js
+++ frontend/src/services/Room.service.js
@@ -1,4 +1,4 @@
-import { api } from "../api";
+import { api, apiMultipart } from "../api";
 
 export default class RoomService {
   static async getAllRooms() {
@@ -11,6 +11,25 @@ export default class RoomService {
     const response = await api.get(`/pokoje/${id}`);
 
     return response.data;
+  }
+
+  static async createNewRoom(roomData) {
+    const formData = new FormData();
+
+    for (const key of Object.keys(roomData.zdjecia)) {
+      formData.append("zdjecia", roomData.zdjecia[key]);
+    }
+
+    formData.append("dostepnosc", roomData.dostepnosc);
+    formData.append("dataZwolnienia", roomData.dataZwolnienia);
+    formData.append("cena", roomData.cena);
+    formData.append("typPokoju", roomData.typPokoju);
+    formData.append("wyposazenie", roomData.wyposazenie);
+    formData.append("ileOsob", roomData.ileOsob);
+
+    const response = await apiMultipart.post(`/pokoje`, formData);
+
+    return response;
   }
 
   static async deleteRoom(id) {
