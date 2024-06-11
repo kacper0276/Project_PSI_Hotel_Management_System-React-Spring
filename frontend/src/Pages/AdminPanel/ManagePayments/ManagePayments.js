e88ec1baacc0ca2a0476789c8c4e88b8f0fb54b2
kacper0276 <kacper0276@op.pl>
2024-06-11 16:15:59 +0200

    [FE] Display payments in admin panel

--- frontend/src/Pages/AdminPanel/ManagePayments/ManagePayments.js
+++ frontend/src/Pages/AdminPanel/ManagePayments/ManagePayments.js
@@ -1,10 +1,60 @@
+import { useEffect, useState } from "react";
 import useWebsiteTitle from "../../../hooks/useWebsiteTitle";
 import styles from "./ManagePayments.module.css";
+import PaymentService from "../../../services/Payment.service";
+import cutTimeInDateTime from "../../../helpers/cutTimeInDateTime";
 
 export default function ManagePayments() {
   useWebsiteTitle("Zarządzaj płatnościami");
+  const [payments, setPayments] = useState([]);
+
+  async function fetchPayments() {
+    setPayments(await PaymentService.getAllPayments());
+  }
+
+  useEffect(() => {
+    fetchPayments();
+  }, []);
 
   return (
-    <div className={`${styles.main_container}`}>Zarządzaj płatnościami</div>
+    <div className={`${styles.main_container}`}>
+      <h1>Zarządzaj płatnościami</h1>
+      <table className={`${styles.data_table}`}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Metoda płatności</th>
+            <th>Status płatności</th>
+            <th>Kwota</th>
+            <th>Data płatności</th>
+            <th>ID rezerwacji</th>
+          </tr>
+        </thead>
+        <tbody>
+          {payments.map((payment, key) => {
+            return (
+              <tr key={key}>
+                <td>{payment.id}</td>
+                <td>{payment.metodaPlatnosci}</td>
+                <td>{payment.statusPlatnosci}</td>
+                <td>{payment.kwota} zł</td>
+                <td>
+                  {payment.dataPlatnosci
+                    ? cutTimeInDateTime(payment.dataPlatnosci)
+                    : "Brak płatności"}
+                </td>
+                <td>{payment.rezerwacje_id}</td>
+                <td>
+                  <button>Edytuj dane</button>
+                </td>
+                <td>
+                  <button>Usuń</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
