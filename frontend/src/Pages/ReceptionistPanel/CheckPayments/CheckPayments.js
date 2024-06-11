c195e8c6122958478373950f7f4b4921f0b43603
kacper0276 <kacper0276@op.pl>
2024-06-11 16:17:38 +0200

    [FE] Display payments in receptionist panel

--- frontend/src/Pages/ReceptionistPanel/CheckPayments/CheckPayments.js
+++ frontend/src/Pages/ReceptionistPanel/CheckPayments/CheckPayments.js
@@ -2,6 +2,7 @@ import styles from "./CheckPayments.module.css";
 import useWebsiteTitle from "../../../hooks/useWebsiteTitle";
 import { useEffect, useState } from "react";
 import PaymentService from "../../../services/Payment.service";
+import cutTimeInDateTime from "../../../helpers/cutTimeInDateTime";
 
 export default function CheckPayments() {
   useWebsiteTitle("Sprawdź płatności");
@@ -15,5 +16,39 @@ export default function CheckPayments() {
     fetchPayments();
   }, []);
 
-  return <div className={`${styles.main_container}`}>Sprawdź płatności</div>;
+  return (
+    <div className={`${styles.main_container}`}>
+      <h1>Sprawdź płatności</h1>
+      <table className={`${styles.data_table}`}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Metoda płatności</th>
+            <th>Status płatności</th>
+            <th>Kwota</th>
+            <th>Data płatności</th>
+            <th>ID rezerwacji</th>
+          </tr>
+        </thead>
+        <tbody>
+          {payments.map((payment, key) => {
+            return (
+              <tr key={key}>
+                <td>{payment.id}</td>
+                <td>{payment.metodaPlatnosci}</td>
+                <td>{payment.statusPlatnosci}</td>
+                <td>{payment.kwota} zł</td>
+                <td>
+                  {payment.dataPlatnosci
+                    ? cutTimeInDateTime(payment.dataPlatnosci)
+                    : "Brak płatności"}
+                </td>
+                <td>{payment.rezerwacje_id}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
