88e0a97e6fb18aceecc8e58b1c22bd6f320bcea3
kacper0276 <kacper0276@op.pl>
2024-07-19 00:37:44 +0200

    Init basic template

---
+++ mobileApp/src/navigation/AppNavigator.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+  Profile: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const AppNavigator = () => {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AppNavigator;
---
+++ mobileApp/src/screens/HomeScreen.tsx
@@ -0,0 +1,36 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+  Profile: undefined;
+};
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+const HomeScreen: React.FC<Props> = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default HomeScreen;
