787c7b2d6338358a01457b59bbd6af2e4fddaa2d
kacper0276 <kacper0276@op.pl>
2024-08-03 11:08:09 +0200

    [MB] Quick fix

--- mobileApp/src/App.tsx
+++ mobileApp/src/App.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationOptions,
+} from '@react-navigation/native-stack';
 import {Screens, StackParamList} from './const/Navigation';
 import {Home} from './screens';
 
 const Stack = createNativeStackNavigator<StackParamList>();
 
+const blankPageStyle: NativeStackNavigationOptions = {
+  headerShown: false,
+};
+
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName={Screens.HOME}>
         <Stack.Group>
           <Stack.Screen
             name={Screens.HOME}
             component={Home}
-            options={{title: 'Home'}}
+            options={{...blankPageStyle, title: 'Home'}}
           />
         </Stack.Group>
       </Stack.Navigator>
