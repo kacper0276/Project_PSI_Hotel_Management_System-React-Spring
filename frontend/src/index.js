7cc7da603a959ba8f0b1b82c48308dabeb27df6e
kacper0276 <kacper0276@op.pl>
2024-08-05 00:07:05 +0200

    [FE] Add component to display notifications

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,13 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./i18n";
+import { ToastContainer } from "react-bootstrap";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
   </React.StrictMode>
 );
 
