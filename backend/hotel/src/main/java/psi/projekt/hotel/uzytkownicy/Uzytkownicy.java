7580cbade5dcbd3dbb1dce34a22555cc41bec0b0
kacper0276 <kacper0276@op.pl>
2024-08-06 02:05:38 +0200

    [BE] Changes in użytkownicy package imports

--- backend/hotel/src/main/java/psi/projekt/hotel/uzytkownicy/Uzytkownicy.java
+++ backend/hotel/src/main/java/psi/projekt/hotel/uzytkownicy/Uzytkownicy.java
@@ -1,4 +1,4 @@
-package psi.projekt.hotel.entity;
+package psi.projekt.hotel.uzytkownicy;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
--- backend/hotel/src/main/java/psi/projekt/hotel/uzytkownicy/UzytkownicyRepository.java
+++ backend/hotel/src/main/java/psi/projekt/hotel/uzytkownicy/UzytkownicyRepository.java
@@ -2,7 +2,6 @@ package psi.projekt.hotel.uzytkownicy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import psi.projekt.hotel.entity.Uzytkownicy;
 import psi.projekt.hotel.entity.enumValue.RolaUzytkownika;
 
 import java.util.List;
