8fcd6663bfb04dc3386226fd9961315e34d56936
kacper0276 <kacper0276@op.pl>
2024-08-07 02:10:49 +0200

    [FE] write main as tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./i18n";
+import { ToastContainer } from "react-bootstrap";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
   </React.StrictMode>
 );
